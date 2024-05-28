2902f2bf0fafa92b350c87ee5db84ed69ae9e212
z wei <zile@243125.xyz>
2024-05-27 17:46:02 -0700

    gpt4 modified

--- agents/prompts.py
+++ agents/prompts.py
@@ -1,6 +1,5 @@
 from langchain.prompts import PromptTemplate
 
-
 ZEROSHOT_REACT_INSTRUCTION = """Collect information for a query plan using interleaving 'Thought', 'Action', and 'Observation' steps. Ensure you gather valid information related to transportation, dining, attractions, and accommodation. All information should be written in Notebook, which will then be input into the Planner tool. Note that the nested use of tools is prohibited. 'Thought' can reason about the current situation, and 'Action' can have 8 different types:
 (1) FlightSearch[Departure City, Destination City, Date]:
 Description: A flight information retrieval tool.
@@ -56,18 +55,24 @@ Query: {query}{scratchpad}"""
 
 
 zeroshot_react_agent_prompt = PromptTemplate(
-                        input_variables=["query", "scratchpad"],
-                        template=ZEROSHOT_REACT_INSTRUCTION,
-                        )
+    input_variables=["query", "scratchpad"],
+    template=ZEROSHOT_REACT_INSTRUCTION,
+)
 
-LANGFUN_INSTRUCTION="""You are a proficient planner. Based on the provided information and query, please give me a detailed plan, including specifics such as flight numbers (e.g., F0123456), restaurant names, and accommodation names. Note that all the information in your plan should be derived from the provided data. You must adhere to the format given in the example. Additionally, all details should align with commonsense. The symbol '-' indicates that information is unnecessary. For example, in the provided sample, you do not need to plan after returning to the departure city. When you travel to two cities in one day, you should note it in the 'Current City' section as in the example (i.e., from A to B).
+LANGFUN_INSTRUCTION = """You are a proficient planner. Based on the provided information and query, please give me a detailed plan, including specifics such as flight numbers (e.g., F0123456), restaurant names, and accommodation names. Note that all the information in your plan should be derived from the provided data. You must adhere to the format given in the example. Additionally, all details should align with commonsense. The symbol '-' indicates that information is unnecessary. For example, in the provided sample, you do not need to plan after returning to the departure city. When you travel to two cities in one day, you should note it in the 'Current City' section as in the example (i.e., from A to B).
 Given information: {text}
 Query: {query}
 Travel Plan:
 """
 
-PLANNER_INSTRUCTION = """You are a proficient planner. Based on the provided information and query, please give me a detailed plan, including specifics such as flight numbers (e.g., F0123456), restaurant names, and accommodation names. Note that all the information in your plan should be derived from the provided data. You must adhere to the format given in the example. Additionally, all details should align with commonsense. The symbol '-' indicates that information is unnecessary. For example, in the provided sample, you do not need to plan after returning to the departure city. When you travel to two cities in one day, you should note it in the 'Current City' section as in the example (i.e., from A to B).
-
+PLANNER_INSTRUCTION = """You are a proficient planner. You are given a travel planning query reference information for the travel plan in CSV format. Your task is to output the travel plan.
+1. Include specifics such as flight numbers (e.g., F0123456), restaurant names, and hotel names. 
+2. All the information in your plan should be derived from the provided reference information. You must adhere to the format given in the example. 
+3. All details should align with common sense. For example, attraction visits and meals are expected to be diverse; you can see which attractions and restaurants have been visited in the current state. 
+4. The symbol '-' indicates that information is unnecessary. For example, in the provided sample, you do not need to plan after returning to the departure city. When you travel to two cities in one day, you should note it in the 'Current City' section as in the example (i.e., from A to B).
+5. When choosing accommodations, ensure that the number of nights stayed at that accommodation is at least the minimum number of nights given in the reference information.
+6. Ensure that the final day of the plan returns to the origin city. Only return to the origin city on the last day; Do not return to the origin city on any of the other days.
+7. Do not visit the same city or attraction twice in the duration of the trip.
 ***** Example *****
 Query: Could you create a travel plan for 7 people from Ithaca to Charlotte spanning 3 days, from March 8th to March 14th, 2022, with a budget of $30,200?
 Travel Plan:
@@ -102,7 +107,8 @@ Accommodation: -
 
 Given information: {text}
 Query: {query}
-Travel Plan:"""
+Travel Plan:
+"""
 
 COT_PLANNER_INSTRUCTION = """You are a proficient planner. Based on the provided information and query, please give me a detailed plan, including specifics such as flight numbers (e.g., F0123456), restaurant names, and hotel names. Note that all the information in your plan should be derived from the provided data. You must adhere to the format given in the example. Additionally, all details should align with common sense. Attraction visits and meals are expected to be diverse. The symbol '-' indicates that information is unnecessary. For example, in the provided sample, you do not need to plan after returning to the departure city. When you travel to two cities in one day, you should note it in the 'Current City' section as in the example (i.e., from A to B). 
 
@@ -180,7 +186,7 @@ You must use Finish to indict you have finished the task. And each action only c
 Given information: {text}
 Query: {query}{scratchpad} """
 
-REFLECTION_HEADER = 'You have attempted to give a sub plan before and failed. The following reflection(s) give a suggestion to avoid failing to answer the query in the same way you did previously. Use them to improve your strategy of correctly planning.\n'
+REFLECTION_HEADER = "You have attempted to give a sub plan before and failed. The following reflection(s) give a suggestion to avoid failing to answer the query in the same way you did previously. Use them to improve your strategy of correctly planning.\n"
 
 REFLECT_INSTRUCTION = """You are an advanced reasoning agent that can improve based on self refection. You will be given a previous reasoning trial in which you were given access to an automatic cost calculation environment, a travel query to give plan and relevant information. Only the selection whose name and city match the given information will be calculated correctly. You were unsuccessful in creating a plan because you used up your set number of reasoning steps. In a few sentences, Diagnose a possible reason for failure and devise a new, concise, high level plan that aims to mitigate the same failure. Use complete sentences.  
 
@@ -281,36 +287,36 @@ Travel Plan:
 """
 
 planner_agent_prompt = PromptTemplate(
-                        input_variables=["text","query"],
-                        template = PLANNER_INSTRUCTION,
-                        )
+    input_variables=["text", "query"],
+    template=PLANNER_INSTRUCTION,
+)
 
 cot_planner_agent_prompt = PromptTemplate(
-                        input_variables=["text","query"],
-                        template = COT_PLANNER_INSTRUCTION,
-                        )
+    input_variables=["text", "query"],
+    template=COT_PLANNER_INSTRUCTION,
+)
 
 react_planner_agent_prompt = PromptTemplate(
-                        input_variables=["text","query", "scratchpad"],
-                        template = REACT_PLANNER_INSTRUCTION,
-                        )
+    input_variables=["text", "query", "scratchpad"],
+    template=REACT_PLANNER_INSTRUCTION,
+)
 
 reflect_prompt = PromptTemplate(
-                        input_variables=["text", "query", "scratchpad"],
-                        template = REFLECT_INSTRUCTION,
-                        )
+    input_variables=["text", "query", "scratchpad"],
+    template=REFLECT_INSTRUCTION,
+)
 
 react_reflect_planner_agent_prompt = PromptTemplate(
-                        input_variables=["text", "query", "reflections", "scratchpad"],
-                        template = REACT_REFLECT_PLANNER_INSTRUCTION,
-                        )
+    input_variables=["text", "query", "reflections", "scratchpad"],
+    template=REACT_REFLECT_PLANNER_INSTRUCTION,
+)
 
 langfun_planner_agent_prompt = PromptTemplate(
-                        input_variables=["text","query"],
-                        template = LANGFUN_INSTRUCTION,
-                        )
+    input_variables=["text", "query"],
+    template=LANGFUN_INSTRUCTION,
+)
 
 langfun_day_by_day_agent_prompt = PromptTemplate(
-                        input_variables=["text","query","current_state"],
-                        template = DAY_BY_DAY_INSTRUCTION,
-                        )
+    input_variables=["text", "query", "current_state"],
+    template=DAY_BY_DAY_INSTRUCTION,
+)
