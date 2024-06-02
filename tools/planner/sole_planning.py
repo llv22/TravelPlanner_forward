a11fad957721179b335db80a5760ea5eed4f347d
z wei <zile@243125.xyz>
2024-06-02 15:05:06 -0700

    fix merge

--- tools/planner/sole_planning.py
+++ tools/planner/sole_planning.py
@@ -2,41 +2,54 @@ import ast
 import os
 import re
 import sys
+
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "../..")))
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-from agents.prompts import langfun_day_by_day_agent_prompt, planner_agent_prompt, cot_planner_agent_prompt, react_planner_agent_prompt,react_reflect_planner_agent_prompt,reflect_prompt
+import argparse
+
 # from utils.func import get_valid_name_city,extract_before_parenthesis, extract_numbers_from_filenames
 import json
 import pickle
 import time
-from langchain.callbacks import get_openai_callback
 
-from tqdm import tqdm
-from tools.planner.apis import Planner, ReactPlanner, ReactReflectPlanner, ByDayPlanner
 import openai
-import argparse
 from datasets import load_dataset
+from langchain.callbacks import get_openai_callback
+from tqdm import tqdm
 
-
+from agents.prompts import (
+    cot_planner_agent_prompt,
+    langfun_day_by_day_agent_prompt,
+    planner_agent_prompt,
+    react_planner_agent_prompt,
+    react_reflect_planner_agent_prompt,
+    reflect_prompt,
+)
+from tools.planner.apis import ByDayPlanner, Planner, ReactPlanner, ReactReflectPlanner
 
 
 def load_line_json_data(filename):
     data = []
-    with open(filename, 'r', encoding='utf-8') as f:
-        for line in f.read().strip().split('\n'):
+    with open(filename, "r", encoding="utf-8") as f:
+        for line in f.read().strip().split("\n"):
             unit = json.loads(line)
             data.append(unit)
     return data
 
+
 def extract_numbers_from_filenames(directory):
     # Define the pattern to match files
-    pattern = r'annotation_(\d+).json'
+    pattern = r"annotation_(\d+).json"
 
     # List all files in the directory
     files = os.listdir(directory)
 
     # Extract numbers from filenames that match the pattern
-    numbers = [int(re.search(pattern, file).group(1)) for file in files if re.match(pattern, file)]
+    numbers = [
+        int(re.search(pattern, file).group(1))
+        for file in files
+        if re.match(pattern, file)
+    ]
 
     return numbers
 
@@ -55,6 +68,7 @@ def catch_openai_api_error():
     else:
         print("API error:", error)
 
+
 def convert_reference_information(reference_information):
     items = ast.literal_eval(reference_information)
     output = ""
@@ -69,13 +83,12 @@ def convert_reference_information(reference_information):
         # if(item[-1] != '}'):
         #     item = item + '}'
 
-        description = item['Description']
-        content = item['Content']
+        description = item["Description"]
+        content = item["Content"]
 
         output += f"{description}\n```csv\n{content}\n```\n"
-    
-    return output
 
+    return output
 
 
 if __name__ == "__main__":
@@ -90,59 +103,118 @@ if __name__ == "__main__":
     parser.add_argument("--output_dir", type=str, default="./")
     parser.add_argument("--strategy", type=str, default="direct")
     args = parser.parse_args()
-    directory = f'{args.output_dir}/{args.set_type}'
-    if args.set_type == 'validation':
-        query_data_list  = load_dataset('osunlp/TravelPlanner','validation')['validation']
-    elif args.set_type == 'test':
-        query_data_list  = load_dataset('osunlp/TravelPlanner','test')['test']
-    numbers = [i for i in range(1,len(query_data_list)+1)]
+    directory = f"{args.output_dir}/{args.set_type}"
+    if args.set_type == "validation":
+        query_data_list = load_dataset("osunlp/TravelPlanner", "validation")[
+            "validation"
+        ]
+    elif args.set_type == "test":
+        query_data_list = load_dataset("osunlp/TravelPlanner", "test")["test"]
+    numbers = [i for i in range(1, len(query_data_list) + 1)]
 
-    if args.strategy == 'direct':
+    if args.strategy == "direct":
         planner = Planner(model_name=args.model_name, agent_prompt=planner_agent_prompt)
-    elif args.strategy == 'by_day':
-        planner = ByDayPlanner(model_name=args.model_name, agent_prompt=langfun_day_by_day_agent_prompt)
-    elif args.strategy == 'cot':
-        planner = Planner(model_name=args.model_name, agent_prompt=cot_planner_agent_prompt)
-    elif args.strategy == 'react':
-        planner = ReactPlanner(model_name=args.model_name, agent_prompt=react_planner_agent_prompt)
-    elif args.strategy == 'reflexion':
-        planner = ReactReflectPlanner(model_name=args.model_name, agent_prompt=react_reflect_planner_agent_prompt,reflect_prompt=reflect_prompt)
-
+    elif args.strategy == "by_day":
+        planner = ByDayPlanner(
+            model_name=args.model_name, agent_prompt=langfun_day_by_day_agent_prompt
+        )
+    elif args.strategy == "cot":
+        planner = Planner(
+            model_name=args.model_name, agent_prompt=cot_planner_agent_prompt
+        )
+    elif args.strategy == "react":
+        planner = ReactPlanner(
+            model_name=args.model_name, agent_prompt=react_planner_agent_prompt
+        )
+    elif args.strategy == "reflexion":
+        planner = ReactReflectPlanner(
+            model_name=args.model_name,
+            agent_prompt=react_reflect_planner_agent_prompt,
+            reflect_prompt=reflect_prompt,
+        )
 
     with get_openai_callback() as cb:
         for number in tqdm(numbers[:]):
-            if os.path.exists(os.path.join(f'{args.output_dir}/{args.model_name}_{args.set_type}/sole-planning/generated_plan_{number}.pkl')):
-                print("Skipping ",number)
+            if os.path.exists(
+                os.path.join(
+                    f"{args.output_dir}/{args.model_name}_{args.set_type}/sole-planning/generated_plan_{number}.pkl"
+                )
+            ) or os.path.exists(
+                os.path.join(
+                    f"{args.output_dir}/{args.model_name}_{args.set_type}/sole-planning/generated_plan_{number}.json"
+                )
+            ):
+                print("Skipping ", number)
                 continue
 
-            query_data = query_data_list[number-1]
-            reference_information = query_data['reference_information']
+            query_data = query_data_list[number - 1]
+            reference_information = query_data["reference_information"]
             while True:
-                    if args.strategy in ['react','reflexion']:
-                        planner_results, scratchpad  = planner.run(reference_information, query_data['query'])
-                    if args.strategy in ['by_day']:
-                        reference_information = convert_reference_information(reference_information)
-                        planner_results  = planner.run(reference_information, query_data['query'])
-                    else:
-                        planner_results  = planner.run(reference_information, query_data['query'])
-                    if planner_results != None:
-                        break
+                if args.strategy in ["react", "reflexion"]:
+                    planner_results, scratchpad = planner.run(
+                        reference_information, query_data["query"]
+                    )
+                if args.strategy in ["by_day"]:
+                    reference_information = convert_reference_information(
+                        reference_information
+                    )
+                    planner_results = planner.run(
+                        reference_information, query_data["query"]
+                    )
+                else:
+                    planner_results = planner.run(
+                        reference_information, query_data["query"]
+                    )
+                if planner_results != None:
+                    break
             print(planner_results)
             # check if the directory exists
-            if not os.path.exists(os.path.join(f'{args.output_dir}/{args.model_name}_{args.set_type}/sole-planning')):
-                os.makedirs(os.path.join(f'{args.output_dir}/{args.model_name}_{args.set_type}/sole-planning'))
-            if not os.path.exists(os.path.join(f'{args.output_dir}/{args.model_name}_{args.set_type}/sole-planning/generated_plan_{number}.pkl')):
-                result =  [{}]
+            if not os.path.exists(
+                os.path.join(
+                    f"{args.output_dir}/{args.model_name}_{args.set_type}/sole-planning"
+                )
+            ):
+                os.makedirs(
+                    os.path.join(
+                        f"{args.output_dir}/{args.model_name}_{args.set_type}/sole-planning"
+                    )
+                )
+            if not os.path.exists(
+                os.path.join(
+                    f"{args.output_dir}/{args.model_name}_{args.set_type}/sole-planning/generated_plan_{number}.pkl"
+                )
+            ):
+                result = [{}]
             else:
-                result = json.load(open(os.path.join(f'{args.output_dir}/{args.model_name}_{args.set_type}/sole-planning/generated_plan_{number}.pkl')))
-            if args.strategy in ['react','reflexion']:
-                result[-1][f'{args.model_name}_{args.strategy}_sole-planning_results_logs'] = scratchpad 
-            result[-1][f'{args.model_name}_{args.strategy}_sole-planning_results'] = planner_results
+                result = json.load(
+                    open(
+                        os.path.join(
+                            f"{args.output_dir}/{args.model_name}_{args.set_type}/sole-planning/generated_plan_{number}.pkl"
+                        )
+                    )
+                )
+            if args.strategy in ["react", "reflexion"]:
+                result[-1][
+                    f"{args.model_name}_{args.strategy}_sole-planning_results_logs"
+                ] = scratchpad
+            result[-1][
+                f"{args.model_name}_{args.strategy}_sole-planning_results"
+            ] = planner_results
             # write to json file
-            if(args.model_name=="langfun"):
-                with open(os.path.join(f'{args.output_dir}/{args.model_name}_{args.set_type}/sole-planning/generated_plan_{number}.pkl'), 'wb') as f:
+            if args.model_name == "langfun":
+                with open(
+                    os.path.join(
+                        f"{args.output_dir}/{args.model_name}_{args.set_type}/sole-planning/generated_plan_{number}.pkl"
+                    ),
+                    "wb",
+                ) as f:
                     pickle.dump(planner_results, f)
             else:
-                with open(os.path.join(f'{args.output_dir}/{args.model_name}_{args.set_type}/sole-planning/generated_plan_{number}.json'), 'w') as f:
+                with open(
+                    os.path.join(
+                        f"{args.output_dir}/{args.model_name}_{args.set_type}/sole-planning/generated_plan_{number}.json"
+                    ),
+                    "w",
+                ) as f:
                     json.dump(result, f, indent=4)
         print(cb)
